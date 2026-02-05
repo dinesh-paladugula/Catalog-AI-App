28f6ffac49c1f3f368b269aa849af5f437b86e50
dinesh-paladugula <dineshpaladugula2002@gmail.com>
2026-02-05 22:45:06 +0530

    Update configuration variables

--- rag_app/core/config.py
+++ rag_app/core/config.py
@@ -12,4 +12,7 @@ TESSERACT_CMD = os.getenv("TESSERACT_CMD")
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 HUGGINGFACE_EMBED_MODEL = os.getenv("HUGGINGFACE_EMBED_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
 
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_MODEL = os.getenv("GROQ_MODEL", "llama-3.1-8b-instant")
+
 IMAGE_OUT_DIR = os.getenv("IMAGE_OUT_DIR", "storage/images")
