12ebe54abb2392b99157ea7cbb9d0564837ce303
dinesh-paladugula <dineshpaladugula2002@gmail.com>
2026-02-05 18:44:23 +0530

    Add central config loader

--- rag_app/core/config.py
+++ rag_app/core/config.py
@@ -9,6 +9,7 @@ MONGODB_COLLECTION = os.getenv("MONGODB_COLLECTION","chunks")
 
 TESSERACT_CMD = os.getenv("TESSERACT_CMD") 
 
+HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 HUGGINGFACE_EMBED_MODEL = os.getenv("HUGGINGFACE_EMBED_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
 
 IMAGE_OUT_DIR = os.getenv("IMAGE_OUT_DIR", "storage/images")
