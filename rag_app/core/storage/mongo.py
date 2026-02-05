00538704dbf1a481b6e129d1ef1d1ef07e12289a
dinesh-paladugula <dineshpaladugula2002@gmail.com>
2026-02-05 18:45:33 +0530

    Add MongoDB connection helper

--- rag_app/core/storage/mongo.py
+++ rag_app/core/storage/mongo.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
+import certifi
+
 from rag_app.core.config import MONGODB_URI, MONGODB_DB, MONGODB_COLLECTION
 
 def get_collection():
     if not MONGODB_URI:
         raise ValueError("MONGODB_URI is not set")
-    client = MongoClient(MONGODB_URI)
+
+    client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
     return client[MONGODB_DB][MONGODB_COLLECTION]
